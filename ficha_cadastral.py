d0f25e87b810fb8a421d66d9c553434b1a1d9db5
Pedro Henrique G. Bertanhi <96213611+pedrobertanhi@users.noreply.github.com>
2025-09-13 17:06:17 -0300

    Update ficha_cadastral.py

--- ficha_cadastral.py
+++ ficha_cadastral.py
@@ -1,7 +1,7 @@
 print("Cadastro de Doadores de Sangues")
 nome = input("Por favor, informe seu nome completo: ")
 peso = float(input("Por favor, informe o seu peso: "))
-altura = int(input("Por favor, informe a sua altura"))
+altura = int(input("Por favor, informe a sua altura: "))
 ano_nascimento = int(input("Por favor, informe seu ano de nascimento: "))
 
 idade = 2025 - ano_nascimento
@@ -9,3 +9,5 @@ tem_peso_minimo = peso > 50
 tem_idade_minima = idade >= 16
 
 texto_saida = f"\tNOME: {nome}\n \tPESO: {peso} kg\n \tALTURA: {altura} cm\n \tIDADE: {idade}\n \tTEM PESO MINIMO PARA DOAR: {tem_peso_minimo}\n \tTEM IDADE MINIMA PARA DOAR: {tem_idade_minima}: "
+
+print(texto_saida)
