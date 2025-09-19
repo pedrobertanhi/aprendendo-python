0fed8e9f796fabf5a0fe9fd7fb7da2c178aea788
Pedro Henrique G. Bertanhi <96213611+pedrobertanhi@users.noreply.github.com>
2025-09-19 15:57:21 -0300

    Create set.py

---
+++ set.py
@@ -0,0 +1,13 @@
+conjunto = set()
+print(type(conjunto))
+
+lista = ["Pedro", "Henrique", "Bruno", "Pedro"]
+print(lista)
+
+conjunto2 = set(lista)
+print(conjunto2)
+
+conjunto3 = {"Cebolinha", "Magali", "Monica", "Cebolinha"}
+print(conjunto3)
+
+#CONTINUA AINDA 5:00
