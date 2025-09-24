d7b9b393ffc9273cc5c72946746424b2af566171
Pedro Henrique G. Bertanhi <96213611+pedrobertanhi@users.noreply.github.com>
2025-09-24 15:50:07 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -1,1 +1,11 @@
+dicionario = {
+    "Nome": "Star Wars - Episódio IV - uma nova esperança",
+    "Diretor": "George Lucas",
+    "Lançamento": 1977,
+    "Bilheteria": 765640000.00
+}
+print(type(dicionario))
 
+print(dicionario)
+
+print(dicionario["Nome"])
