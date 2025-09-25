ebb45ef33ba25734de65486218cee1d0b9003b15
Pedro Henrique G. Bertanhi <96213611+pedrobertanhi@users.noreply.github.com>
2025-09-25 15:48:29 -0300

    Create ficha_cadastral_av.py

---
+++ ficha_cadastral_av.py
@@ -0,0 +1,27 @@
+op = 0
+ficha = {}
+
+while op != 4:
+    print("\n FICHA CADASTRAL")
+    print("1 - Incluir informação")
+    print("2 - Recuperar informação")
+    print("3 - Exibir ficha completa")
+    print("4 - Sair")
+    
+    op = int(input("Informe uma opção: "))
+    
+    if op == 1:
+        #Inserir Ficha
+        
+    
+    elif op == 2:
+        #Recuperar dados
+        
+    elif op == 3:
+        #Exibir ficha completa
+        
+    elif op == 4:
+        print("Finalizando Sistema")
+        break
+    else:
+        print("Selecione uma opção valida")
