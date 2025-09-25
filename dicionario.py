90dee97ca99f5469d082f0f4ad2185726f1b7e82
Pedro Henrique G. Bertanhi <96213611+pedrobertanhi@users.noreply.github.com>
2025-09-25 15:38:14 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -10,4 +10,34 @@ print(dicionario)
 
 print(dicionario["Nome"])
 
-###### PAREI NO 6 MIN
+# Inserir novo valor
+
+dicionario["Gênero"] = "Space Opera"
+print(dicionario)
+
+#KEYS
+print(dicionario.keys())
+
+for chave in dicionario.keys():
+    print(chave)
+    
+    
+# VALUES
+print(dicionario.values())
+for valor in dicionario.values():
+    print(valor)
+    
+#ITEMS
+print(dicionario.items())
+for chave, valor in dicionario.items():
+    print(f"{chave} -- {valor}")
+    
+# GET (Procura no dicionario)
+print(dicionario.get("público")) # N EXISTE
+print(dicionario.get("nome")) # EXISTE
+
+#SETDEFAULT
+dicionario.setdefault("púlico", 1000)
+print(dicionario)
+dicionario.setdefault("público", 9000)
+print(dicionario)
